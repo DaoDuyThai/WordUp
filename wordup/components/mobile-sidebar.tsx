00a9389990dcfb72ae49e171a12fd37000b9cf8c
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-06 06:33:01 +0700

    finish sidebar

--- wordup/components/mobile-sidebar.tsx
+++ wordup/components/mobile-sidebar.tsx
@@ -1,10 +1,10 @@
-import {Sheet, SheetContent, SheetTrigger} from "@/components/ui/sheet";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
 
 import { Sidebar } from "@/components/sidebar";
 import { Menu } from "lucide-react";
 
 export const MobileSidebar = () => {
-    return(
+    return (
         <Sheet>
             <SheetTrigger>
                 <Menu className="text-white" />
