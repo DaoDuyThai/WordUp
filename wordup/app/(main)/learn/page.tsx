b50cc0e072118e0d0e7ed792869ac8d2b4e19263
Đào Duy Thái <thaiddhe176315@fpt.edu.vn>
2024-05-06 07:30:42 +0700

    learn page wrapper

--- wordup/app/(main)/learn/page.tsx
+++ wordup/app/(main)/learn/page.tsx
@@ -1,8 +1,18 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgress } from "@/components/user-progress";
+
 const LearnPage = () => {
     return (
-        <div>
-            <h1>Learn</h1>
-            <p>Welcome to the Learn page!</p>
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgress activeCourse={{ title: "Spanish", imageSrc: "/es.svg" }} hearts={5} points={100} hasActiveSubscription={false} />
+
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish" />
+            </FeedWrapper>
         </div>
     );
 }
